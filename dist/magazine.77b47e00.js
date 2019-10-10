0f1e87653c51378c0c2e6e34ed109b8a7b180089
Sarah Davidsen <sarahdavidsen@desktop-s1gg866.kea.local>
2019-10-10 09:03:29 +0200

    almost finished

--- dist/magazine.77b47e00.js
+++ dist/magazine.77b47e00.js
@@ -200,11 +200,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49625" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59289" + '/');
->>>>>>> 20a21eeedf2342150f4a1e3ab4742cdc3f259161
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54401" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
